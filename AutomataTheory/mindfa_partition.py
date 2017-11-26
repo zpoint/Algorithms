390be233839b24c2b62119c6c28f30a891682c8b
zpoint <zp0int@qq.com>
2017-11-26 22:39:35 +0800

    mindfa_partition 0.8

--- AutomataTheory/mindfa_partition.py
+++ AutomataTheory/mindfa_partition.py
@@ -33,16 +33,29 @@ class DfaTable(Table):
                 if not new_sets:
                     new_sets.append({state})
                     continue
+                same_partition = False
                 for new_each_set in new_sets:
                     new_set_state = None
                     for _ in new_each_set:
                         new_set_state = _
                         break
                     states_a, states_b = self.entry[state], self.entry[new_set_state]
-                    same_partition = True
                     for state_a, state_b in zip(states_a, states_b):
-                        pass
-
+                        for each_set in states_sets:
+                            if state_a in each_set:
+                                if state_b in each_set:
+                                    same_partition = True
+                                    break
+                            elif state_b in each_set:
+                                if state_a in each_set:
+                                    same_partition = True
+                                    break
+                    if same_partition:
+                        new_each_set.add(state)
+                        break
+                if not same_partition:
+                    new_sets.append({state})
+        return new_sets
 
     def minimize_partition(self):
         prev_sets = list()
@@ -61,3 +74,6 @@ class DfaTable(Table):
                 break
             # continue partition prev_set
             curr_sets = self.partiton(prev_sets)
+            if curr_sets == prev_sets:
+                break
+            prev_sets = curr_sets
