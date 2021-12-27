33db9ab78945d7d3ecc468cfcde8639571c34f6d
zpoint <zp0int@qq.com>
2021-12-27 11:19:40 +0800

    Update generate-parentheses.cpp

--- LeetCode/generate-parentheses.cpp
+++ LeetCode/generate-parentheses.cpp
@@ -12,6 +12,25 @@ std::vector<std::string> vec3 = {"((()))","(()())","(())()","()(())","()()()"};
 
 class Solution {
 public:
+    void helper(int begin, int close, int n, std::vector<string> &ret, std::string& tmp) {
+        if (begin == n and close == n) {
+            ret.push_back(tmp);
+            return;
+        }
+
+        if (begin < n) {
+            tmp.push_back('(');
+            helper(begin+1, close, n, ret, tmp);
+            tmp.pop_back();
+        }
+
+        if (close < n and close < begin) {
+            tmp.push_back(')');
+            helper(begin, close+1, n, ret, tmp);
+            tmp.pop_back();
+        }
+    }
+
     vector<string> generateParenthesis(int n) {
         if (n == 1) {
             return vec1;
@@ -20,48 +39,9 @@ public:
         } else if (n == 3) {
             return vec3;
         }
-        std::vector<std::string> vec_new = vec3;
-        std::set<std::string> set_new;
-        auto vec_old = vec_new;
-        vec_new.reserve(3 * (n - 1) - 1);
-        vec_old.reserve(3 * (n - 1) - 1);
-
-        for (int i = 3; i < n; ++i) {
-            int index = 0;
-            int vec_old_size = vec_old.size();
-            for (const auto& str : vec_old) {
-                const auto left = "()" + str;
-                const auto right = str + "()";
-                const auto mid = "(" + str + ")";
-
-                if (set_new.find(left) == set_new.end()) {
-                    if (index < vec_old_size) {
-                        vec_new[index++] = left;
-                    } else {
-                        vec_new.push_back(left);
-                    }
-                    set_new.insert(left);
-                }
-
-
-                if (index < vec_old_size) {
-                    vec_new[index++] = mid;
-                } else {
-                    vec_new.push_back(mid);
-                }
-                set_new.insert(mid);
-
-                if (set_new.find(right) == set_new.end()) {
-                   if (index < vec_old_size) {
-                        vec_new[index++] = right;
-                    } else {
-                        vec_new.push_back(right);
-                    }
-                    set_new.insert(right);
-                }
-            }
-            vec_old = vec_new;
-        }
-        return vec_new;
+        std::vector<string> vec_end;
+        std::string tmp;
+        helper(0, 0, n, vec_end, tmp);
+        return vec_end;
     }
 };
